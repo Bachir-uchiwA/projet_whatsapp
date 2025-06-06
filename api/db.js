57ad0e16c142e970cf0f4aa47f88e7bee06ac372
Uchiwa75567 <pabassdiame76@gmail.com>
2025-06-06 11:50:01 +0000

    second-test

--- api/db.js
+++ api/db.js
@@ -2,6 +2,10 @@ import { promises as fs } from 'fs';
 import path from 'path';
 
 export default async function handler(req, res) {
+  if (req.method !== 'GET') {
+    res.status(405).json({ error: 'Method Not Allowed. Read-only API on Vercel.' });
+    return;
+  }
   const filePath = path.join(process.cwd(), 'db.json');
   const data = await fs.readFile(filePath, 'utf8');
   res.setHeader('Content-Type', 'application/json');
