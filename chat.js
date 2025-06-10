745df1753b3e9835f68393ed32737c3818272ebf
Uchiwa75567 <pabassdiame76@gmail.com>
2025-06-10 17:13:14 +0000

    fourthy2-test

--- chat.js
+++ chat.js
@@ -20,7 +20,6 @@ document.addEventListener('DOMContentLoaded', function() {
             sidebarChats.classList.remove('hidden');
         });
     }
-    // ...autres fonctionnalités JS éventuelles...
 });
 
 export {};
